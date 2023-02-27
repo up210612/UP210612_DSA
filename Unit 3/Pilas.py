c4453a33dc6ff497ad54bbba5f22f1495ead6eef
UP2101612 <up210612@alumnos.upa.edu.mx>
2023-02-27 12:39:35 -0600

    up

---
+++ Unit 3/Pilas.py
@@ -0,0 +1,26 @@
+class Node: 
+    def __init__(self, data):
+        self.data =data
+        self.next= None
+    
+    def getData(self):
+        return self.data
+
+class Stack: #las clases siempre comienzan con MAYUSC 
+    def __init__(self):
+        self.head=None
+        self.size=0
+    
+    def getSize(self):
+        return self.size
+
+    def isEmpty(self):
+        
+        return True if self.size == 0 else False
+        #return True if not self.size else False 
+        #return True if not self.head else False 
+
+    def push (self, data):
+        newNode = Node(data)
+        newNode.next= self.head
+        self.head= newNode
--- Unit 3/nodo.py
+++ Unit 3/nodo.py
@@ -13,3 +13,19 @@ nodo1= node("jesus")
 print(nodo1.data)
 print(nodo1.getData())
 print(nodo1.next)
+
+nodo1.setData("Maria")
+print(nodo1.getData())
+print(nodo1.data)
+nodo2= node("Jose")
+
+nodo1.next=nodo2
+print(nodo1.next.data)
+
+nodo3= node("jesus")
+nodo2.next=nodo3
+
+print("----->")
+print(nodo1.data)
+print(nodo1.next.data)
+print(nodo1.next.next.data)
